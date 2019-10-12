3f63fa4e1e7c02e11ce0fbffa3601ae788915f7d
iannisimo <iannisimo@gmail.com>
2019-10-12 16:18:56 +0200

    Java es2

--- PRL2/java/Esercitazione1/src/test.java
+++ PRL2/java/Esercitazione1/src/test.java
@@ -1,12 +1,17 @@
 public class test {
     public static void main(String[] args) {
+
+
         ModStringCollection msc = new ModStringCollection();
+
         msc.insert(new ModString("ciao"));
         msc.insert(new ModString("ciao"));
-        msc.insert(new ModString("miao"));
+        msc.insert(new ModString("ciao"));
+        msc.insert(new ModString("ciao"));
+
         System.out.println(msc.occurrences(new ModString("ciao")));
-        System.out.println(msc.occurrences(new ModString("miao")));
-        System.out.println(msc.occurrences(new ModString("test")));
+        msc.remove(new ModString("ciao"), 0);
+        System.out.println(msc.occurrences(new ModString("ciao")));
         String[] mscS = msc.getUnique();
         for(String s : mscS) {
             System.out.println(s);
